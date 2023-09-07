6c24cdb24b055ca730a90522ae6ae3b0f45bf73a
AgusVJ <agustinvillanuevajousset@gmail.com>
2023-09-07 20:08:02 -0300

    adaptados parrafos a la actualidad

--- js/main.js
+++ js/main.js
@@ -27,7 +27,3 @@ collapseParagraph.innerHTML = personalDescription;
 
 const myModal = document.getElementById("myModal");
 const myInput = document.getElementById("myInput");
-
-myModal.addEventListener("shown.bs.modal", () => {
-  myInput.focus();
-});
